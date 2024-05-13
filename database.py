347a174dca67e2e12e0d1eab07fb613ace479624
Tirza Gabriella <tirzagabriella20@gmail.com>
2024-05-13 16:36:02 +0700

    change to postgre

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://default:niHEv8qfwGP6@ep-cool-salad-a1i5tldt.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
